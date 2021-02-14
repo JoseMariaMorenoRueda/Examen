aa96f205ef3604f61bbd5e512e98dd5f271ca2a8
Jose <alumor7618@ieselcaminas.org>
2021-02-14 17:09:09 +0100

    Clase Main

---
+++ src/primera/Main.java
@@ -0,0 +1,17 @@
+package primera;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Deck d = new Deck();
+        d.shuffle();
+        System.out.println(d);
+        Card c1 = d.extractCard();
+        Card c2 = d.extractCard();
+        System.out.println(c1);
+        System.out.println(c2);
+        d.writeToFile("cards.csv");
+        Deck d2 = Deck.readFromFile("cards.csv");
+        System.out.println(d2);
+    }
+}
